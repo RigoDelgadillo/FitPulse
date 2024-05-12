716a701153e92796fdce32dba7ae320e7e568581
Rigo Delgadillo <rigodelgadillo10@gmail.com>
2024-05-12 13:05:02 -0600

    Se generó un controlador de administrador y se agregaron las vistas de este controlador

---
+++ PIAProWeb/PIAProWeb/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PIAProWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult CRUDUsuarios()
+        {
+            return View();
+        }
+        public IActionResult CRUDRutinas()
+        {
+            return View();
+        }
+        public IActionResult ReporteDeVentas()
+        {
+            return View();
+        }
+        public IActionResult MenuAdmin()
+        {
+            return View();
+        }
+    }
+}
