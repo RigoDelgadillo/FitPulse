8e00250b3b254e1991b4618ac789f09185048979
David23MA <luis2000atilano2308@gmail.com>
2024-05-20 13:21:14 -0600

    se agrego el diseño a la pantalla de crear

--- PIAProWeb/PIAProWeb/obj/Debug/net6.0/PIAProWeb.AssemblyInfo.cs
+++ PIAProWeb/PIAProWeb/obj/Debug/net6.0/PIAProWeb.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PIAProWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec24c4532eeba6695303376ce57bdfaa520d31a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b49285bac83a081e8d15261e2f1a61a41dc9783c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PIAProWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PIAProWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
